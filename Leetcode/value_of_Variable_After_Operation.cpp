8bbb8a9ce7c76a6285fad1fa9619d71694f7fc30
sheraz61 <hsheraz271@gmail.com>
2025-01-12 23:22:18 +0500

    add solution for calculating final value after operations leetcode 2011

---
+++ Leetcode/value_of_Variable_After_Operation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ int finalValueAfterOperations(vector<string>& operations) {
+       int count=0;
+        for(int i=0;i<operations.size();i++)
+        {
+            if(operations[i]=="X++"|| operations[i]=="++X"){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return count;
+    }
+int main(){
+vector<string> operations={"X++","++X","--X","X--"};
+cout<<finalValueAfterOperations(operations);
+}
