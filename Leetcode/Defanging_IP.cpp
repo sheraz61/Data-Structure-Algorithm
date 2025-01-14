3656e46625f5b6a85ee877e69e4d8dc96584f0fb
sheraz61 <hsheraz271@gmail.com>
2025-01-15 00:03:36 +0500

    refactor defangIPaddr function to use a loop for better clarity and add a main function for testing

--- Leetcode/Defanging_IP.cpp
+++ Leetcode/Defanging_IP.cpp
@@ -1,3 +1,19 @@
- string defangIPaddr(string address) {
-        return regex_replace(address, regex("[.]"), "[.]");
+#include<iostream>
+#include<regex>
+#include<string>
+using namespace std;
+string defangIPaddr(string address) {
+        string ans = "";
+        for(auto i : address){
+            if(i != '.')
+                ans += i;
+            else
+                ans += "[.]";
+        }
+        return ans;
     }
+int main(){
+string address = "192.168.1.1";
+cout<<defangIPaddr(address);
+}
+ 
