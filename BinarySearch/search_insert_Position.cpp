cea37fffe95b06300a0b41fbdf8e6a312157fca2
sheraz61 <hsheraz271@gmail.com>
2025-01-24 23:38:55 +0500

    add solution for search insert position using lower bound

---
+++ BinarySearch/search_insert_Position.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ int lowerBound(vector<int>& nums, int target, int n) {
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+int main(){
+  vector<int> nums={1,2,4,7};
+  int target=9;
+  int result=lowerBound(nums,target,nums.size());
+  cout<<result;
+  return 0;
+}
