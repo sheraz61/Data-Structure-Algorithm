1ab4c0c80bad84ccbb87d0bd367d03493b474c5c
sheraz61 <hsheraz271@gmail.com>
2025-01-24 23:56:09 +0500

    add solution to find floor and ceil of a target in a sorted array

---
+++ BinarySearch/FloorAndSeil.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int findFloor(int arr[], int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if (arr[mid] <= x) {
+			ans = arr[mid];
+			//look for smaller index on the left
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1; // look on the right
+		}
+	}
+	return ans;
+}
+
+int findCeil(int arr[], int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if (arr[mid] >= x) {
+			ans = arr[mid];
+			//look for smaller index on the left
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1; // look on the right
+		}
+	}
+	return ans;
+}
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+	int f = findFloor(arr, n, x);
+	int c = findCeil(arr, n, x);
+	return make_pair(f, c);
+}
+int main()
+{
+    int nums[] = {10,20,30,40,50};
+    int target = 25;
+    pair<int, int> result = getFloorAndCeil(nums, 5, target);
+    cout << "Floor: " << result.first << " Ceil: " << result.second << endl;
+    return 0;
+}
