d65fef0bfab433bc4a48f6c2cb3a675c8c70ddcb
sheraz61 <hsheraz271@gmail.com>
2025-03-01 16:51:31 +0500

    refactor BST functions for consistency and add root-to-leaf path functionality

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 class Node
 {
 public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
     Node(int val)
     {
         this->data = val;
         left = right = NULL;
     }
 };
-Node* insert(Node* root, int val)
+Node *insert(Node *root, int val)
 {
     if (root == NULL)
     {
@@ -28,7 +29,7 @@ Node* insert(Node* root, int val)
     }
     return root;
 }
-void inorder(Node* root)
+void inorder(Node *root)
 {
     if (root == NULL)
     {
@@ -38,9 +39,9 @@ void inorder(Node* root)
     cout << root->data << " ";
     inorder(root->right);
 }
-Node* buildBST(int arr[], int n)
+Node *buildBST(int arr[], int n)
 {
-    Node* root = NULL;
+    Node *root = NULL;
     for (int i = 0; i < n; i++)
     {
         root = insert(root, arr[i]);
@@ -48,34 +49,79 @@ Node* buildBST(int arr[], int n)
     return root;
 }
 // Print in Range
-void printInRange(Node*root,int st,int en){
-    if(root==NULL){
+void printInRange(Node *root, int st, int en)
+{
+    if (root == NULL)
+    {
         return;
     }
-    if(st<=root->data && root->data<en){
-        cout<<root->data<<" ";
-        printInRange(root->left,st,en);
-        printInRange(root->right,st,en);
-    }else if(root->data<st){
-        printInRange(root->right,st,en);
-    }else{
-        printInRange(root->left,st,en);
+    if (st <= root->data && root->data < en)
+    {
+        cout << root->data << " ";
+        printInRange(root->left, st, en);
+        printInRange(root->right, st, en);
+    }
+    else if (root->data < st)
+    {
+        printInRange(root->right, st, en);
+    }
+    else
+    {
+        printInRange(root->left, st, en);
     }
 }
-//inorder successor
-Node* IS(Node* root){
-    while(root->left!=NULL){
-        root=root->left;
+// inorder successor
+Node *IS(Node *root)
+{
+    while (root->left != NULL)
+    {
+        root = root->left;
     }
     return root;
+}
+// root to leaf path
+void printPath(vector<int> &path)
+{
+    for (int i = 0; i < path.size(); i++)
+    {
+        cout << path[i] << " ";
+    }
+    cout << endl;
+}
+void pathHelper(Node *root, vector<int> &path)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    path.push_back(root->data);
+    if (root->left == NULL && root->right == NULL)
+    {
+        printPath(path);
+        path.pop_back();
+        return;
+    }
+    pathHelper(root->left, path);
+    pathHelper(root->right, path);
+    path.pop_back();
+}
+void rootToLeafPath(Node *root)
+{
+    vector<int> path;
+    pathHelper(root, path);
 }
 int main()
 {
     int arr[] = {5, 1, 3, 4, 2, 7, 6, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    Node* root = buildBST(arr, n);
+    Node *root = buildBST(arr, n);
     inorder(root);
-    cout<<endl;
-    printInRange(root,2,6);
+    cout << endl;
+    printInRange(root, 2, 6);
+    cout << endl;
+    Node *temp = IS(root);
+    cout << "Inorder Successor of 3 is : " << temp->data << endl;
+    rootToLeafPath(root);
+
     return 0;
 }
